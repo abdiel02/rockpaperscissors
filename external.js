b7a3635e77251d68ca6f95dea0bbbdffffeda793
abdiel02 <121787602+abdiel02@users.noreply.github.com>
2023-01-29 22:27:22 -0800

    changes to html and javascript

--- external.js
+++ external.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
             function() {
                 var playersel = $(this).val();
                 var computersel = "";
+                let playerScore = 0;
+                let computerScore = 0;
                 $("#mymsg").html("");
                 var computerplay = Math.floor(Math.random() * 3);
                 //computer play random selection of 0-2, 0, 1, or 2.
@@ -19,27 +21,37 @@ $(document).ready(function() {
                 $("#computerdisplay").html(computersel);
                 
                 
-                for (let i = 0; i < 5; i++) {
+               // for (let i = 0; i < 5; i++) {
                     //playerselection and computer selection is same
                     if(playersel == "rock" && computersel == "rock" ||
                         playersel == "paper" && computersel == "paper" ||
                         playersel == "scissors" && computersel == "scissors") {
-                            $("#gameresult").html("tie!");
+                            $("#gameresult").html("Tie");
+                            playerScore++;
+                            computerScore++;
+                            $("#playscore").html(playerScore);
+                            $("#compscore").html(computerScore);
                     
                     }
                     //player beats computer
                     else if(playersel == "rock" && computersel == "scissors" ||
                             playersel == "paper" && computersel == "rock" ||
                             playersel == "scissors" && computersel == "paper") {
-                                $("#gameresult").html(playersel + " beats " + computersel);
+                                $("#gameresult2").html(playersel + " beats " + computersel);
+                                $("#gameresult").html("You Won!");
+                                playerScore++;
+                                $("#playscore").html(playerScore);
                             }
                     //computer beats player
                     else if(playersel == "rock" && computersel == "scissors" ||
                             playersel == "paper" && computersel == "scissors" ||
                             playersel == "scissors" && computersel == "rock") {
-                                $("#gameresult").html(computersel + " beats " + playersel);
+                                $("#gameresult2").html(computersel + " beats " + playersel);
+                                $("#gameresult").html("You Lost");
+                                computerScore++;
+                                $("#compscore").html(computerScore);
                             }
-                };
+            //  };
 
             });
 
